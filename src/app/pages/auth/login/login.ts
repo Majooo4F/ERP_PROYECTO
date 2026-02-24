8b528dc7bdcb05833f32cc2ed46ab889823147c2
Majooo4F <majoflores734@gmail.com>
2026-02-23 22:52:01 -0600

    PRACTICA3

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button'; 
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterLink, ToastModule, ButtonModule],
+  providers: [MessageService],
+  templateUrl: './login.html'
 })
 export class Login {
+  loginForm: FormGroup;
+  private readonly VALID_USER = {
+    email: 'admin@practica.com',
+    password: 'Password123!'
+  };
 
+  constructor(private fb: FormBuilder, private messageService: MessageService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      if (email === this.VALID_USER.email && password === this.VALID_USER.password) {
+        this.messageService.add({ 
+          severity: 'success', 
+          summary: 'Éxito', 
+          detail: 'Login correcto. ¡Bienvenido!' 
+        });
+      } else {
+        this.messageService.add({ 
+          severity: 'error', 
+          summary: 'Error', 
+          detail: 'Correo o contraseña incorrectos' 
+        });
+      }
+    }
+  }
 }
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 import { RouterLink } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, ToastModule, RouterLink, InputTextModule,
+  ButtonModule],
+  providers: [MessageService],
+  templateUrl: './register.html'
 })
-export class Register {
+export class Register implements OnInit {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      usuario: ['', Validators.required],
+      nombreCompleto: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      direccion: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      fechaNacimiento: ['', [Validators.required, this.validarEdad]],
+      password: ['', [
+        Validators.required, 
+        Validators.minLength(10),
+        Validators.pattern('^(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,}$')
+      ]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.compararPasswords });
+  }
+  validarEdad(control: AbstractControl) {
+    if (!control.value) return null;
+    const fechaNac = new Date(control.value);
+    const hoy = new Date();
+    let edad = hoy.getFullYear() - fechaNac.getFullYear();
+    const mes = hoy.getMonth() - fechaNac.getMonth();
+    
+    if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+      edad--;
+    }
+    return edad >= 18 ? null : { menorDeEdad: true };
+  }
+  compararPasswords(group: AbstractControl) {
+    const pass = group.get('password')?.value;
+    const confirm = group.get('confirmPassword')?.value;
+    return pass === confirm ? null : { notMatching: true };
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      this.messageService.add({ 
+        severity: 'success', 
+        summary: 'Registro Exitoso', 
+        detail: 'Tu cuenta ha sido creada (Localmente)' 
+      });
+      console.log('Datos enviados:', this.registerForm.value);
+    } else {
+      this.messageService.add({ 
+        severity: 'warn', 
+        summary: 'Formulario Inválido', 
+        detail: 'Por favor, revisa los campos marcados en rojo' 
+      });
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
